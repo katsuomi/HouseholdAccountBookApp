001727be36419f0875ddcf0276cf4b28b4b9c701
katsuomi <>
2019-05-23 11:01:16 +0900

    firebaseとの接続とreduxの導入

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,19 +1,20 @@
 import firebase from 'firebase';
 import 'firebase/auth';
 
+export const db = firebase.firestore();
+export const functions = firebase.functions();
+
 const config = {
-    apiKey: "AIzaSyDg2FdVLjviUHF3nu4gjApRuf1iqfV-rdo",
+    apiKey: functions.config().app.key,
     authDomain: "household-account-book-app.firebaseapp.com",
     databaseURL: "https://household-account-book-app.firebaseio.com",
     projectId: "household-account-book-app",
     storageBucket: "household-account-book-app.appspot.com",
     messagingSenderId: "349017346788",
-    appId: "1:349017346788:web:a415514b36da79cb"
+    appId: functions.config().app.id
   };
   // Initialize Firebase
   firebase.initializeApp(config);
 
-export const db = firebase.firestore();
-export const functions = firebase.functions();
 
 export default firebase;
